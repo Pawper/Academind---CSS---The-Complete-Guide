cc3ef3966090affca21c24ae20c36454ba9cca55
Phillip A. Wessels <phillip.a.wessels@gmail.com>
2019-11-04 00:50:10 -0800

    9.136 complete

--- basics-01-understanding-the-course-project-starting-code/shared.js
+++ basics-01-understanding-the-course-project-starting-code/shared.js
@@ -1,6 +1,7 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var selectPlanButtons = document.querySelectorAll('.plan button');
+var modalNoButton = document.querySelector('.modal__action--negative');
 
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
@@ -8,4 +9,12 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
     modal.style.display = 'block';
     backdrop.style.display = 'block';
   });
+}
+
+backdrop.addEventListener('click', closeModal);
+modalNoButton.addEventListener('click', closeModal);
+
+function closeModal() {
+  modal.style.display = 'none';
+  backdrop.style.display = 'none';
 }
