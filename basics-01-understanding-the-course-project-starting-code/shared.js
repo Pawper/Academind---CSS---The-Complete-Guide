3bf42cd3c9971dfbccc2e239e049a3cc55207b3b
Phillip A. Wessels <phillip.a.wessels@gmail.com>
2020-04-05 15:42:56 -0700

    Section 16 Complete

--- basics-01-understanding-the-course-project-starting-code/shared.js
+++ basics-01-understanding-the-course-project-starting-code/shared.js
@@ -3,7 +3,8 @@ var modal = document.querySelector('.modal');
 var selectPlanButtons = document.querySelectorAll('.plan button');
 var modalNoButton = document.querySelector('.modal__action--negative');
 var toggleButton = document.querySelector('.toggle-button');
-var mobileNav = document.querySelector('.mobile-nav')
+var mobileNav = document.querySelector('.mobile-nav');
+var ctaButton = document.querySelector('.main-nav__item--cta');
 
 // console.dir(backdrop.style['background-image']);
 
@@ -51,4 +52,14 @@ toggleButton.addEventListener('click', function() {
   setTimeout(function() {
     backdrop.classList.add('open');
   }, 10)
+})
+
+ctaButton.addEventListener('animationstart', function() {
+  console.log('Animation start', event);
+})
+ctaButton.addEventListener('animationend', function() {
+  console.log('Animation ended', event);
+})
+ctaButton.addEventListener('animationiteration', function() {
+  console.log('Animation iteration', event);
 })
